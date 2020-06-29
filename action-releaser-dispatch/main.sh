59ebb348f7dfc3de76551ba474cccef2821a7b53
Ilir Bekteshi <Ilir.Bekteshi@qlik.com>
2020-06-29 10:31:30 +0200

    Use GITHUB_REF as condition for running releaser-dispatch

--- action-releaser-dispatch/main.sh
+++ action-releaser-dispatch/main.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -l
 set -euo pipefail
 
-GH_REPO=${GITHUB_REPOSITORY#*/}
-
-if ! echo $VERSION | grep -E "^[0-9]+\.[0-9]+\.[0-9]+$"; then
-    echo "Version: $VERSION does not match semver for release version of X.Y.Z"
-    exit 1
+if ! echo $GITHUB_REF | grep -E 'refs/tags/v[0-9]+\.[0-9]+\.[0-9]+$'; then
+    echo "This is not a release tag, skip."
+    exit 0
+else
+    VERSION=${GITHUB_REF##*/}
 fi
+
+GH_REPO=${GITHUB_REPOSITORY#*/}
 
 body_template='{"event_type":"pre-release","client_payload":{"repository":"%s","tag":"%s"}}'
 body=$(printf $body_template "$GH_REPO" "$VERSION")
