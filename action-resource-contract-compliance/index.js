5f3cf1dae4f15349f58b21e4d4c4e06fd554cfb8
Mats Jacobsson <mats.jacobsson@qlik.com>
2020-09-22 19:48:53 +0200

    wip

--- action-resource-contract-compliance/index.js
+++ action-resource-contract-compliance/index.js
@@ -1,16 +1,20 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-//const fs = require('fs');
-//const yaml = require('js-yaml');
+const fs = require('fs');
+const yaml = require('js-yaml');
 
 try {
-    //let fileContents = fs.readFileSync('./data-multi.yaml', 'utf8');
-    //let data = yaml.safeLoadAll(fileContents);
+    const chartDir = process.env.CHART_DIR || core.getInput('chart_dir');
+    const qlikcommonVersion = process.env.QLIKCOMMON_VERSION || core.getInput('qlikcommon_version');
+    const fileContents = fs.readFileSync(`${chartDir}/requirements.yaml`, 'utf8');
 
-    const nameToGreet = core.getInput('artifactory_user');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+    // Check if qlikcommon is listed as dependency.
+    const {dependencies} = yaml.safeLoadAll(fileContents)[0];
+    const {version} = dependencies.find(dep => dep.name == 'qlikcommon');
+    if (version) {
+        console.log(`Found qlikcommon version: ${version}`);
+        console.log(`Requires qlikcommon version: ${qlikcommonVersion}`);
+    }
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
