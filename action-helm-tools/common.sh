9c7016f6c32d6831fb516fa174b392e196b774e6
Mats Jacobsson <mats.jacobsson@qlik.com>
2020-09-23 14:12:17 +0200

    WIP

--- action-helm-tools/common.sh
+++ action-helm-tools/common.sh
@@ -80,16 +80,15 @@ yaml_lint() {
 }
 
 check_resource_contract_compliance () {
+    echo "==> Resource contract compliance test"
+    
+    qlikcommon_version=$(yq r $CHART_DIR/requirements.yaml 'dependencies.(name==qlikcommon).version') || echo "ERROR: $CHART_DIR/requirements.yaml not found" | exit 1
 
-  qlikcommon_version=$(yq r $CHART_DIR/requirements.yaml 'dependencies.(name==qlikcommon).version') || echo "ERROR: $CHART_DIR/requirements.yaml not found" | exit 1
-
-  if [[ -z "$qlikcommon_version" ]]; then
-    echo "ERROR: Chart is not using qlikcommon base chart (resource contract)"
-    echo "INFO: Please convert chart to use resource contract as per the standards set here: https://github.com/qlik-trial/resource-contract"
-    exit 1
-  elif [[ "$qlikcommon_version" != "$LATEST_QLIKCOMMON_VERSION" ]]; then
-      echo "ERROR: Chart is not using the latest qlikcommon chart version"
-      echo "INFO: Please update qlikcommon version in requirements.yaml to $LATEST_QLIKCOMMON_VERSION"
-      exit 1
-  fi
+    if [[ -z "$qlikcommon_version" ]]; then
+        echo "ERROR: Please convert chart to use resource contract as per the standards set here: https://github.com/qlik-trial/resource-contract"
+        exit 1
+    elif [[ "$qlikcommon_version" != "$LATEST_QLIKCOMMON_VERSION" ]]; then
+        echo "ERROR: Please update to latest qlikcommon version in requirements.yaml to $LATEST_QLIKCOMMON_VERSION"
+        exit 1
+    fi
 }
