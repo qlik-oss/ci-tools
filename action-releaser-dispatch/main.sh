3dfe629bb743a990fe7648d8b2bfb6619973e485
Ilir Bekteshi <Ilir.Bekteshi@qlik.com>
2020-09-29 13:52:26 +0200

    Test input version for semver format (instead of refs)

--- action-releaser-dispatch/main.sh
+++ action-releaser-dispatch/main.sh
@@ -1,19 +1,22 @@
 #!/bin/bash -l
 set -euo pipefail
 
-if ! echo $GITHUB_REF | grep -E 'refs/tags/v[0-9]+\.[0-9]+\.[0-9]+$'; then
+# Strip v prefix
+VERSION=${VERSION#v}
+
+if ! echo $VERSION | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$'; then
     echo "This is not a release tag, skip."
     exit 0
-else
-    VERSION=${GITHUB_REF##*/}
 fi
+
+VERSION="v${VERSION}"
 
 GH_REPO=${GITHUB_REPOSITORY#*/}
 
 body_template='{"event_type":"pre-release","client_payload":{"repository":"%s","tag":"%s"}}'
 body=$(printf $body_template "$GH_REPO" "$VERSION")
 
-curl --location --request POST "${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/dispatches" \
+curl --fail --location --request POST "${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/dispatches" \
     --header "Authorization: token ${GH_PAT}" \
     --header "Content-Type: application/json" \
     --data ${body}
