118158791f149793a977c5f83f9c41f5c70ef673
Mats Jacobsson <mats.jacobsson@qlik.com>
2020-10-01 19:41:48 +0200

    Add curl

--- action-slack-notification/main.sh
+++ action-slack-notification/main.sh
@@ -9,3 +9,5 @@ cat ${INPUT_REPOSITORY}/components.yaml
 url="$MASTER_NODE.pte.qlikdev.com"
 msg="Cluster setup for $INPUT_REPOSITORY completed\nMaster node: $MASTER_NODE.qliktech.com\nKeycloak: https://keycloak.$url\nArgoCD: https://argocd.$url\nDocker registry: registry.$url\nTraefik dashboard: https://traefik.$url/dashboard/"
 echo "Message: $msg"
+
+curl --fail -H 'Content-Type: application/json' -d "{\"text\": \"$msg\", \"channel\": \"@mats.jacobsson\"}" $INPUT_SLACK_WEBHOOK
