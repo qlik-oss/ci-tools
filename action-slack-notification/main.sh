b93f09cf09fa96327f91e8691d6a407583e0adb3
Mats Jacobsson <mats.jacobsson@qlik.com>
2020-10-01 20:04:30 +0200

    Parse components.yaml

--- action-slack-notification/main.sh
+++ action-slack-notification/main.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 echo "Repo: ${INPUT_REPOSITORY}"
 echo "Webhook: ${INPUT_SLACK_WEBHOOK}"
 
-cat ${INPUT_REPOSITORY}/components.yaml
+channel=$(yq r ${INPUT_REPOSITORY}/components.yaml 'components[0].botSlackChannel')
 
 url="$MASTER_NODE.pte.qlikdev.com"
 msg="Cluster setup for $INPUT_REPOSITORY completed\nMaster node: $MASTER_NODE.qliktech.com\nKeycloak: https://keycloak.$url\nArgoCD: https://argocd.$url\nDocker registry: registry.$url\nTraefik dashboard: https://traefik.$url/dashboard/"
 echo "Message: $msg"
 
-curl --fail -H 'Content-Type: application/json' -d "{\"text\": \"$msg\", \"channel\": \"@mats.jacobsson\"}" $INPUT_SLACK_WEBHOOK
+curl --fail -H 'Content-Type: application/json' -d "{\"text\": \"$msg\", \"channel\": \"$channel\"}" $INPUT_SLACK_WEBHOOK
