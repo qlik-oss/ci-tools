ca81196401637484a8bf97c737cbff4389dbcb14
Mats Jacobsson <mats.jacobsson@qlik.com>
2020-10-01 16:54:15 +0200

    Github action for Slack notification

---
+++ action-slack-notification/main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+set -euo pipefail
+
+echo "Repo: ${INPUT_REPOSITORY}"
+echo "Webhook: ${INPUT_SLACK_WEBHOOK}"
+
+cat ${INPUT_REPOSITORY}/components.yaml
+
+url="$MASTER_NODE.pte.qlikdev.com"
+msg="Cluster setup for $INPUT_REPOSITORY completed\nMaster node: $MASTER_NODE.qliktech.com\nKeycloak: https://keycloak.$url\nArgoCD: https://argocd.$url\nDocker registry: registry.$url\nTraefik dashboard: https://traefik.$url/dashboard/"
+echo "Message: $msg"
