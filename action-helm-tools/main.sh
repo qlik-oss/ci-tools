7b9d974d7cc198172d6980289078d679271b35e7
Ilir Bekteshi <Ilir.Bekteshi@qlik.com>
2020-10-27 19:04:59 +0100

    Set commit status on workflow dispatch

--- action-helm-tools/main.sh
+++ action-helm-tools/main.sh
@@ -1,23 +1,7 @@
 #!/usr/bin/env bash
+trap 'set_commit_status "error"' ERR
 
-# Copyright The Helm Authors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-set -o errexit
-set -o nounset
-set -o pipefail
-# set -x # Use only to debug the scripts otherwise too verbose
+set -Eeuo pipefail
 
 export SCRIPT_DIR=$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}" || realpath "${BASH_SOURCE[0]}")")
 
@@ -41,4 +25,22 @@ main() {
 
 }
 
+set_commit_status() {
+  STATUS=$1
+
+  if [[ "$GITHUB_EVENT_NAME" == "workflow_dispatch" ]] && [[ -n "$GITHUB_SHA" ]]; then
+    CONTEXT=${GITHUB_WORKFLOW:="Package Helm Chart"}
+    APIURL="${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_SHA}"
+    TARGET_URL="https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
+
+    curl "$APIURL" \
+      -H "Content-Type: application/json" \
+      -H "Authorization: token ${GITHUB_TOKEN}" \
+      -X POST \
+      -d "{\"state\": \"$STATUS\", \"context\": \"$CONTEXT\", \"target_url\": \"$TARGET_URL\"}"
+  fi
+}
+
+set_commit_status "pending"
 main
+set_commit_status "success"
