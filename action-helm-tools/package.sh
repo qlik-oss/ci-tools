33c0575e59d76e68017dc25d3e05c6e8fd8c4f80
Mats Jacobsson <mats.jacobsson@qlik.com>
2020-10-28 12:44:54 +0100

    Add bitnami helm repo

--- action-helm-tools/package.sh
+++ action-helm-tools/package.sh
@@ -3,6 +3,10 @@ set -eo pipefail
 
 source $SCRIPT_DIR/common.sh
 
+public_repos=(
+    "bitnami https://charts.bitnami.com/bitnami"
+)
+
 install_yq
 get_component_properties
 yaml_lint
@@ -11,6 +15,10 @@ install_helm
 helm init --client-only
 echo "==> Helm add repo"
 helm repo add $HELM_LOCAL_REPO $REGISTRY/$HELM_VIRTUAL_REPO --username $ARTIFACTORY_USERNAME --password $ARTIFACTORY_PASSWORD
+for repo in "${public_repos[@]}"; do
+	IFS=" " read -r -a arr <<< "${repo}"
+    helm repo add "${arr[0]}" "${arr[1]}"
+done
 helm repo update
 
 export LATEST_QLIKCOMMON_VERSION=$(helm inspect chart qlik/qlikcommon | yq r - 'version')
