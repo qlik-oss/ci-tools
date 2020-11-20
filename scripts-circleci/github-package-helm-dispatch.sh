353d97a8502eef93ae14a04a6398630236ea3769
Ilir Bekteshi <Ilir.Bekteshi@qlik.com>
2020-11-20 18:24:14 +0100

    Ignore only *released* but not *release* (#62)

--- scripts-circleci/github-package-helm-dispatch.sh
+++ scripts-circleci/github-package-helm-dispatch.sh
@@ -4,7 +4,7 @@ VERSION_FILE=${VERSION_FILE:="/workspace/version.txt"}
 GITHUB_WORKFLOW=${GITHUB_WORKFLOW:="package-helm.yaml"}
 
 if [ -z "${CIRCLE_TAG}" ]; then
-  if [ -z "${CIRCLE_BRANCH##*release*}" ]; then
+  if [ -z "${CIRCLE_BRANCH##*released*}" ]; then
     echo "Skipping ${GITHUB_WORKFLOW} on release branches: ${CIRCLE_BRANCH}"
     exit 0
   fi
