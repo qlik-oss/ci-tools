a963e74706eb0bfb3f6015c239054f9dc5a499a6
Ilir Bekteshi <Ilir.Bekteshi@qlik.com>
2020-12-18 13:40:10 +0100

    [cci] If VERSION is set do not read VERSION_FILE (#66)

--- scripts-circleci/github-package-helm-dispatch.sh
+++ scripts-circleci/github-package-helm-dispatch.sh
@@ -3,6 +3,21 @@
 VERSION_FILE=${VERSION_FILE:="/workspace/version.txt"}
 GITHUB_WORKFLOW=${GITHUB_WORKFLOW:="package-helm.yaml"}
 
+if [ -z "${VERSION}" ]; then
+  VERSION=$(cat "$VERSION_FILE")
+fi
+
+# Final check that version is set
+if [ -z "${VERSION}" ]; then
+  echo "ERROR: VERSION could not be determined"
+  echo "If version file is in different location than ${VERSION_FILE}"
+  echo "before running github_workflow_dispatch.sh script, set a variable:"
+  echo "export VERSION_FILE=/path/version.txt"
+  echo "or set VERSION variable directly'"
+  echo 'export VERSION="$(node|cat ...)"'
+  exit 1
+fi
+
 if [ -z "${CIRCLE_TAG}" ]; then
   if [ -z "${CIRCLE_BRANCH##*released*}" ]; then
     echo "Skipping ${GITHUB_WORKFLOW} on release branches: ${CIRCLE_BRANCH}"
@@ -14,16 +29,6 @@ if [ -n "${CIRCLE_TAG}" ]; then
   REF=${CIRCLE_TAG}
 else
   REF=${CIRCLE_BRANCH}
-fi
-
-_VERSION=$(cat "$VERSION_FILE")
-VERSION=${VERSION:=$_VERSION}
-if [ -z "${VERSION}" ]; then
-  echo "ERROR: VERSION could not be determined"
-  echo "If version file is in different location than ${VERSION_FILE}"
-  echo "before running github_workflow_dispatch.sh script, set a variable:"
-  echo "export VERSION_FILE=/path/version.txt"
-  exit 1
 fi
 
 body_template='{"ref":"%s","inputs":{"version":"%s"}}'
