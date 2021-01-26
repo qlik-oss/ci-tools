6ccc9a82fc269bc2696234b9610aa36532d3faa4
Ilir Bekteshi <Ilir.Bekteshi@qlik.com>
2021-01-26 12:28:17 +0100

    [workflow-dispatch] generic workflow dispatch action

---
+++ action-workflow-dispatch/main.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+set -euo pipefail
+
+if [ -z "$GITHUB_TOKEN" ]; then
+  echo "GITHUB_TOKEN not available"
+  exit 1
+fi
+
+if [ -z "$INPUT_REF" ]; then
+  INPUT_REF=$(gh api "repos/${INPUT_OWNER}/${INPUT_REPOSITORY}" | jq -r .default_branch)
+fi
+
+body_template='{"ref":"%s","inputs":"%s"}'
+body=$(printf $body_template "$INPUT_REF" "$INPUT_INPUTS")
+echo "Using ${body}"
+
+curl --fail --location --request POST "${GITHUB_API_URL}/repos/${INPUT_OWNER}/${INPUT_REPOSITORY}/actions/workflows/${INPUT_WORKFLOW}/dispatches" \
+  --header "Authorization: token ${GITHUB_TOKEN}" \
+  --header "Content-Type: application/json" \
+  --header "Accept: application/vnd.github.v3+json" \
+  --data "${body}"
