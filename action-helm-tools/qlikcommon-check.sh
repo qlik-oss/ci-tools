58f58a38ef83dce6d8a399acaafe8f60325af106
Ilir Bekteshi <Ilir.Bekteshi@qlik.com>
2021-03-31 10:11:45 +0200

    Do not fail if depdendncy updater is enabled

--- action-helm-tools/qlikcommon-check.sh
+++ action-helm-tools/qlikcommon-check.sh
@@ -1,6 +1,7 @@
 #!/bin/env bash
 set -eo pipefail
 
+# Credit https://stackoverflow.com/a/37939589/1331719
 function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
 function majorminor { echo "$@" | awk -F. '{ printf("%d%03d\n", $1,$2); }'; }
 
@@ -10,12 +11,26 @@ LATEST_QLIKCOMMON_VERSION=$(helm inspect chart qlik/qlikcommon | yq e '.version'
 CURRENT_QLIKCOMMON_VERSION=$(helm inspect chart "$CHART_DIR" | yq e '.dependencies[] | select(.name == "qlikcommon") | .version' -)
 
 if [ -z "$CURRENT_QLIKCOMMON_VERSION" ]; then
+
   echo "::warning ::The chart must be converted to Resource Contract"
+
 elif [ $(majorminor $CURRENT_QLIKCOMMON_VERSION) -lt $(majorminor $LATEST_QLIKCOMMON_VERSION) ]; then
-  echo "::error ::You must update qlikcommon to $LATEST_QLIKCOMMON_VERSION or it will fail production approval"
-  exit 1
+
+  MSG="You must update qlikcommon $CURRENT_QLIKCOMMON_VERSION to $LATEST_QLIKCOMMON_VERSION or it will fail production approval"
+  # Allow if major.minor don't match due to dependency updater
+  if [[ "${DEPENDENCY_UPDATE}" == "true" ]]; then
+    echo "::warning ::$MSG"
+  else
+    echo "::error ::$MSG"
+    exit 1
+  fi
+
 elif [ $(version $CURRENT_QLIKCOMMON_VERSION) -lt $(version $LATEST_QLIKCOMMON_VERSION) ]; then
+
   echo "::warning ::Dependency qlikcommon $CURRENT_QLIKCOMMON_VERSION is outdated, update to latest $LATEST_QLIKCOMMON_VERSION version"
+
 else
+
   echo "qlikcommon:$CURRENT_QLIKCOMMON_VERSION is up to date ✔"
+
 fi
