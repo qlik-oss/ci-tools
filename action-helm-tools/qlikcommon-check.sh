fdfe7604201a36ef4a1761be4ca3a56d611ddbe9
Ilir Bekteshi <Ilir.Bekteshi@qlik.com>
2021-03-31 09:59:43 +0200

    Fail if qlikcommon is behind major.minor version

--- action-helm-tools/qlikcommon-check.sh
+++ action-helm-tools/qlikcommon-check.sh
@@ -2,6 +2,7 @@
 set -eo pipefail
 
 function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
+function majorminor { echo "$@" | awk -F. '{ printf("%d%03d\n", $1,$2); }'; }
 
 echo "==> Checking Resource Contract compliance"
 
@@ -10,6 +11,9 @@ CURRENT_QLIKCOMMON_VERSION=$(helm inspect chart "$CHART_DIR" | yq e '.dependenci
 
 if [ -z "$CURRENT_QLIKCOMMON_VERSION" ]; then
   echo "::warning ::The chart must be converted to Resource Contract"
+elif [ $(majorminor $CURRENT_QLIKCOMMON_VERSION) -lt $(majorminor $LATEST_QLIKCOMMON_VERSION) ]; then
+  echo "::error ::You must update qlikcommon to $LATEST_QLIKCOMMON_VERSION or it will fail production approval"
+  exit 1
 elif [ $(version $CURRENT_QLIKCOMMON_VERSION) -lt $(version $LATEST_QLIKCOMMON_VERSION) ]; then
   echo "::warning ::Dependency qlikcommon $CURRENT_QLIKCOMMON_VERSION is outdated, update to latest $LATEST_QLIKCOMMON_VERSION version"
 else
