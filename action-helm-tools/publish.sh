02a245aa7b07ae5701e4437e5c86bd13d03eba03
Magnus Karlsson <48644061+mtk7801@users.noreply.github.com>
2021-09-13 12:50:31 +0200

    Use QLIK_XXXX instead of GHCR_XXXX (#127)
    Co-authored-by: Magnus Karlsson <magnus_karlsson@eu-dyh.qliktech.com>

--- action-helm-tools/publish.sh
+++ action-helm-tools/publish.sh
@@ -6,15 +6,11 @@ get_component_properties
 
 export HELM_EXPERIMENTAL_OCI=1
 
-if [ -z "$GHCR_HELM_DEV_REGISTRY" ]; then
-  GHCR_HELM_DEV_REGISTRY=$QLIK_HELM_DEV_REGISTRY
-fi
-
 echo "==> Publish to GHCR"
 
 echo "====> Saving chart $CHART_NAME:$VERSION"
-helm chart save $CHART_NAME-$VERSION.tgz $GHCR_HELM_DEV_REGISTRY/$CHART_NAME:$VERSION
+helm chart save $CHART_NAME-$VERSION.tgz $QLIK_HELM_DEV_REGISTRY/$CHART_NAME:$VERSION
 
 echo "====> Pushing chart $CHART_NAME:$VERSION to GHCR"
-helm chart push $GHCR_HELM_DEV_REGISTRY/$CHART_NAME:$VERSION
+helm chart push $QLIK_HELM_DEV_REGISTRY/$CHART_NAME:$VERSION
 echo "====> Chart $CHART_NAME:$VERSION pushed to GHCR"
