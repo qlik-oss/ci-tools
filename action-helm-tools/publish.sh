83137cb904d9b3f8f10fa0ff34a0163186bca37d
Ilir Bekteshi <Ilir.Bekteshi@qlik.com>
2022-01-11 09:33:36 +0100

    Helm 3.7 upgrade

--- action-helm-tools/publish.sh
+++ action-helm-tools/publish.sh
@@ -6,11 +6,6 @@ get_component_properties
 
 export HELM_EXPERIMENTAL_OCI=1
 
-echo "==> Publish to GHCR"
-
-echo "====> Saving chart $CHART_NAME:$VERSION"
-helm chart save $CHART_NAME-$VERSION.tgz $QLIK_HELM_DEV_REGISTRY/$CHART_NAME:$VERSION
-
-echo "====> Pushing chart $CHART_NAME:$VERSION to GHCR"
-helm chart push $QLIK_HELM_DEV_REGISTRY/$CHART_NAME:$VERSION
-echo "====> Chart $CHART_NAME:$VERSION pushed to GHCR"
+echo "==> Push chart $CHART_NAME:$VERSION to GHCR"
+helm push "$CHART_NAME-$VERSION.tgz" "oci://$QLIK_HELM_DEV_REGISTRY"
+echo "==> Chart $CHART_NAME:$VERSION pushed to GHCR"
