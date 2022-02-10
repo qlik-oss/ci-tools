8905392f8d649b701468a3aa6c57331f8c862321
Harel Assaf <harel.assaf@attunity.com>
2022-02-10 18:18:05 +0200

    pass docker registry through environment instead of input

--- action-docker-sha256/main.sh
+++ action-docker-sha256/main.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -l
 set -o pipefail
 
+DOCKER_DEV_REGISTRY=${DOCKER_DEV_REGISTRY:=hub.docker.com}
+
 if [ -n "$DOCKER_DEV_PASSWORD" ]; then
   echo "==> Docker registry login"
-  echo "$DOCKER_DEV_PASSWORD" | docker login -u "$DOCKER_DEV_USERNAME" --password-stdin "$INPUT_IMAGE_REGISTRY"
+  echo "$DOCKER_DEV_PASSWORD" | docker login -u "$DOCKER_DEV_USERNAME" --password-stdin "$DOCKER_DEV_REGISTRY"
 fi
 
-IMAGE=${INPUT_IMAGE_REGISTRY}/${INPUT_IMAGE_NAME}:${INPUT_IMAGE_TAG}
+IMAGE=${DOCKER_DEV_REGISTRY}/${INPUT_IMAGE_NAME}:${INPUT_IMAGE_TAG}
 echo calculating sha256 for $IMAGE
 docker pull $IMAGE
 docker inspect $IMAGE | jq '(.[].RepoDigests[0] | split(":"))[1]'
