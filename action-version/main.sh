8910248d0940a0bad9e8fd0f7d67d6105f5222ef
Harel Assaf <rlsf@users.noreply.github.com>
2022-03-03 20:00:53 +0200

    Update main.sh

--- action-version/main.sh
+++ action-version/main.sh
@@ -8,7 +8,7 @@ BRANCH_NAME=""
 git fetch --prune --unshallow || true
 
 # On push event
-if [ "$GITHUB_EVENT_NAME" == "push" ]; then
+if [[ "$GITHUB_EVENT_NAME" == "push" ]] || [[ "$GITHUB_EVENT_NAME" == "issue_comment" ]] ; then
     _sha=$GITHUB_SHA
     echo "${GITHUB_REF}" | grep -E '^refs/heads/' && BRANCH_NAME=${GITHUB_REF##*/}
 fi
@@ -18,9 +18,6 @@ if [ "$GITHUB_EVENT_NAME" == "pull_request" ]; then
     _sha=$(jq -r .pull_request.head.sha "$GITHUB_EVENT_PATH")
     BRANCH_NAME=${GITHUB_HEAD_REF}
 fi
-
-# failover - if no sha created so far, try rev-parse
-_sha=${_sha:=$(git rev-parse --short HEAD)}
 
 # git-describe - Give an object a human readable name based on an available ref
 # On PR actions/checkout checkouts a merge commit instead of commit sha, git describe
