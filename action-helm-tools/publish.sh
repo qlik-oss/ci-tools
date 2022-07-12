1035aa7c36ec6dcba4f9696ef3b9118f2b6f5517
Lars Andersson <lars.andersson@qliktech.com>
2022-07-12 13:33:49 +0200

    Add helm inspect for debugging purposes

--- action-helm-tools/publish.sh
+++ action-helm-tools/publish.sh
@@ -14,3 +14,6 @@ helm chart save $CHART_NAME-$VERSION.tgz $QLIK_HELM_DEV_REGISTRY/$CHART_NAME:$VE
 echo "====> Pushing chart $CHART_NAME:$VERSION to GHCR"
 helm chart push $QLIK_HELM_DEV_REGISTRY/$CHART_NAME:$VERSION
 echo "====> Chart $CHART_NAME:$VERSION pushed to GHCR"
+
+echo "====> Inspecting chart $CHART_NAME:$VERSION (to simplify debugging if needed)"
+helm inspect $CHART_NAME:$VERSION
