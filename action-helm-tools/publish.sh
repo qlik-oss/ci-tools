d8026e0d482366cac97d6763cc161d112733947d
Magnus Karlsson <magnus_karlsson@eu-dyh.qliktech.com>
2022-09-15 10:14:28 +0200

    Remove dependencies to QLIK_XXXX
    The "Package helm" workflow, which uses action-helm-tools, should
    be closer integrated with Qlik Releaser. Therefore, similar to
    Qlik Releaser, action-helm-tools should use environment variables
    GITHUB_USER and GITHUB_TOKEN instead of
    
    QLIK_DOCKER_DEV_USERNAME
    QLIK_HELM_DEV_USERNAME
    QLIK_DOCKER_DEV_PASSWORD
    QLIK_HELM_DEV_PASSWORD
    
    and use hard coded values "ghcr.io/qlik-trial" and
    "ghcr.io/qlik-trial/helm" instead of environment variables
    QLIK_DOCKER_DEV_REGISTRY and QLIK_HELM_DEV_REGISTRY.

--- action-helm-tools/publish.sh
+++ action-helm-tools/publish.sh
@@ -9,8 +9,8 @@ export HELM_EXPERIMENTAL_OCI=1
 echo "==> Publish to GHCR"
 
 echo "====> Saving chart $CHART_NAME:$VERSION"
-helm chart save $CHART_NAME-$VERSION.tgz $QLIK_HELM_DEV_REGISTRY/$CHART_NAME:$VERSION
+helm chart save $CHART_NAME-$VERSION.tgz $HELM_DEV_REGISTRY/$CHART_NAME:$VERSION
 
-echo "====> Pushing chart $CHART_NAME:$VERSION to GHCR"
-helm chart push $QLIK_HELM_DEV_REGISTRY/$CHART_NAME:$VERSION
-echo "====> Chart $CHART_NAME:$VERSION pushed to GHCR"
+echo "====> Pushing chart $CHART_NAME:$VERSION to $HELM_DEV_REGISTRY"
+helm chart push $HELM_DEV_REGISTRY/$CHART_NAME:$VERSION
+echo "====> Chart $CHART_NAME:$VERSION pushed to $HELM_DEV_REGISTRY"
