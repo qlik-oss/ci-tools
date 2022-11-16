a2fec9c87720138b9b5ac0c3df991117f85fdbcb
Harel Assaf <harel.assaf@attunity.com>
2022-11-16 11:06:33 +0200

    fixed helm push syntax

--- action-helm-tools/publish.sh
+++ action-helm-tools/publish.sh
@@ -7,5 +7,5 @@ get_component_properties
 echo "==> Publish to GHCR"
 
 echo "====> Pushing chart $CHART_NAME:$VERSION to $HELM_DEV_REGISTRY"
-helm push $CHART_NAME-$VERSION.tgz oci://$HELM_DEV_REGISTRY/$CHART_NAME:$VERSION
+helm push $CHART_NAME-$VERSION.tgz oci://$HELM_DEV_REGISTRY
 echo "====> Chart $CHART_NAME:$VERSION pushed to $HELM_DEV_REGISTRY"
