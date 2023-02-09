c13100e0c3170a62c326cf13c518d746dc777871
Masaru Hoshi <masaru.hoshi@qlik.com>
2023-02-08 21:52:21 -0500

    Add karpenter helm repo
    Signed-off-by: Masaru Hoshi <masaru.hoshi@qlik.com>

--- action-helm-tools/common.sh
+++ action-helm-tools/common.sh
@@ -106,6 +106,7 @@ add_helm_repos() {
     "twin https://twin.github.io/helm-charts"
     "argo https://argoproj.github.io/argo-helm"
     "dynatrace https://raw.githubusercontent.com/Dynatrace/dynatrace-operator/master/config/helm/repos/stable"
+    "karpenter https://charts.karpenter.sh"
   )
   # "kubed https://charts.appscode.com/stable/"
   # charts.appscode.com currently not working, get builds working angain and figure out what breaks because kubed is missing instead
