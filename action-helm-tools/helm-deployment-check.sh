f7af214921fb1472028fab7e5c7e0a52006dc631
Magnus Karlsson <48644061+mtk7801@users.noreply.github.com>
2023-03-23 09:38:13 +0100

    Update helm-deployment-check.sh
    Print more extensive logs for non-ready pods

--- action-helm-tools/helm-deployment-check.sh
+++ action-helm-tools/helm-deployment-check.sh
@@ -81,7 +81,7 @@ if [[ $deployed -ne 1 ]]; then
     set +e
     logfile="${POD_LOGS}/${pod}.log"
     echo "==> Pod logs: $pod" | tee -a "$logfile"
-    kubectl logs -n "$NAMESPACE" "$pod" --all-containers 2>&1 | tee -a "$logfile"
+    kubectl logs -n "$NAMESPACE" "$pod" -p --all-containers 2>&1 | tee -a "$logfile"
     echo "==> Pod describe: $pod" | tee -a "$logfile"
     kubectl describe pod -n "$NAMESPACE" "$pod" 2>&1 | tee -a "$logfile"
   done
