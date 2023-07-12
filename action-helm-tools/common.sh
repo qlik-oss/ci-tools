e84d1354de1dab4a090d95f28dc5b03ca93874d1
Harel Assaf <harel.assaf@attunity.com>
2023-07-12 18:28:17 +0300

    chore: using k8s 1.24 in helm ci

--- action-helm-tools/common.sh
+++ action-helm-tools/common.sh
@@ -13,10 +13,10 @@ export HELM_DEV_REGISTRY="ghcr.io/qlik-trial/helm"
 
 # Tools
 export HELM_VERSION=${HELM_VERSION:="3.10.2"}
-export KUBECTL_VERSION=${KUBECTL_VERSION:="1.22.15"}
+export KUBECTL_VERSION=${KUBECTL_VERSION:="1.24.7"}
 export KIND_VERSION=${KIND_VERSION:="v0.17.0"}
 # Get Image version from https://github.com/kubernetes-sigs/kind/releases, look for K8s version in the release notes
-export KIND_IMAGE=${KIND_IMAGE:="kindest/node:v1.22.15@sha256:7d9708c4b0873f0fe2e171e2b1b7f45ae89482617778c1c875f1053d4cef2e41"}
+export KIND_IMAGE=${KIND_IMAGE:="kindest/node:v1.24.7@sha256:577c630ce8e509131eab1aea12c022190978dd2f745aac5eb1fe65c0807eb315"}
 export YQ_VERSION="4.25.2"
 
 get_component_properties() {
