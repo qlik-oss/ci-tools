6cabde9df9586686ef1ddccc49ed19fe12cddae8
Dave Smith <david.smith@qlik.com>
2023-07-21 10:13:39 -0700

    add tracing helm dep

--- action-helm-tools/common.sh
+++ action-helm-tools/common.sh
@@ -122,6 +122,7 @@ add_helm_repos() {
     "aws-eks https://aws.github.io/eks-charts"
     "uswitch https://uswitch.github.io/kiam-helm-charts/charts"
     "sealed-secrets https://bitnami-labs.github.io/sealed-secrets"
+    "jaegertracing https://jaegertracing.github.io/helm-charts"
   )
   # "kubed https://charts.appscode.com/stable/"
   # charts.appscode.com currently not working, get builds working angain and figure out what breaks because kubed is missing instead
